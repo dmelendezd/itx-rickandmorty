d12f572e46fa8d0327fd07a13d443e06a0c92ff0
David Meléndez Díez <dmelend9@corp.vodafone.es>
2023-06-23 12:42:02 +0200

    Merge branch feature/dev/integration-characters-list

--- app/src/main/java/com/itxtest/rickandmorty/view/characterslist/view/adapter/CharactersListAdapter.kt
+++ app/src/main/java/com/itxtest/rickandmorty/view/characterslist/view/adapter/CharactersListAdapter.kt
@@ -1,6 +1,9 @@
 package com.itxtest.rickandmorty.view.characterslist.view.adapter
 
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.view.updateLayoutParams
 import androidx.recyclerview.widget.RecyclerView
 import com.itxtest.rickandmorty.R
 import com.itxtest.rickandmorty.model.domain.Character
@@ -30,9 +33,14 @@ class CharactersListAdapter(
 
         val character = characters[position]
         holder.bind(character)
-        val itemSeparation =
-            holder.itemBinding.root.context.resources.getDimension(R.dimen.characters_list_item_separation).toInt()
+
+        val resources = holder.itemBinding.root.context.resources
+        val itemSeparation = resources.getDimension(R.dimen.characters_list_item_separation).toInt()
         holder.itemBinding.root.setMargins(itemSeparation, 0, 0, 0)
+
+        if (resources.getBoolean(R.bool.characters_list_alternate_side_enabled)) {
+            updateImageSide(holder.itemBinding, position)
+        }
 
         holder.itemBinding.lastKnowLocation.setOnClickListener {
             adapterModel.onLastKnownLocationClickCallback?.invoke(character.lastKnownLocation)
@@ -68,6 +76,55 @@ class CharactersListAdapter(
             notifyItemRangeInserted(firstPositionToAdd, addedCount)
         }
     }
+
+    private fun updateImageSide(itemBinding: CharacterItemBinding, position: Int) {
+        itemBinding.imageContainer.updateLayoutParams<ConstraintLayout.LayoutParams> {
+            this.startToStart = ConstraintLayout.LayoutParams.UNSET
+            this.endToEnd = ConstraintLayout.LayoutParams.UNSET
+        }
+        itemBinding.dataContainer.updateLayoutParams<ConstraintLayout.LayoutParams> {
+            this.startToEnd = ConstraintLayout.LayoutParams.UNSET
+            this.endToStart = ConstraintLayout.LayoutParams.UNSET
+        }
+        val constraintSet = ConstraintSet()
+        constraintSet.clone(itemBinding.root)
+        if (isImageLeftSide(position)) {
+            constraintSet.connect(
+                itemBinding.imageContainer.id,
+                ConstraintSet.START,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.START
+            )
+            constraintSet.connect(
+                itemBinding.dataContainer.id,
+                ConstraintSet.START,
+                itemBinding.imageContainer.id,
+                ConstraintSet.END
+            )
+        } else {
+            constraintSet.connect(
+                itemBinding.imageContainer.id,
+                ConstraintSet.END,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.END
+            )
+            constraintSet.connect(
+                itemBinding.dataContainer.id,
+                ConstraintSet.END,
+                itemBinding.imageContainer.id,
+                ConstraintSet.START
+            )
+            constraintSet.connect(
+                itemBinding.dataContainer.id,
+                ConstraintSet.START,
+                ConstraintSet.PARENT_ID,
+                ConstraintSet.START
+            )
+        }
+        constraintSet.applyTo(itemBinding.root)
+    }
+
+    private fun isImageLeftSide(position: Int): Boolean = position % 2 == 0
 
     class CharacterViewHolder(
          val itemBinding: CharacterItemBinding
